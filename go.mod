c80514b88fd529affc92b345e8a4065103e4eb39
TheDiveO <6920158+thediveo@users.noreply.github.com>
2026-06-16 14:16:15 +0000

    fix: no active sessions error when using buildkit
    Signed-off-by: TheDiveO <6920158+thediveo@users.noreply.github.com>

--- go.mod
+++ go.mod
@@ -16,11 +16,12 @@ require (
 	github.com/opencontainers/image-spec v1.1.1
 	github.com/pkg/errors v0.9.1
 	github.com/thediveo/fdooze v0.3.3
-	github.com/thediveo/nonstd v0.15.0
+	github.com/thediveo/nonstd v0.18.0
 	github.com/thediveo/safe v0.9.1
 	github.com/thediveo/success v1.3.1
 	go.uber.org/mock v0.6.0
-	golang.org/x/crypto v0.52.0
+	golang.org/x/crypto v0.53.0
+	golang.org/x/sync v0.21.0
 	google.golang.org/protobuf v1.36.11
 )
 
@@ -83,10 +84,9 @@ require (
 	go.opentelemetry.io/proto/otlp v1.10.0 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
 	golang.org/x/mod v0.36.0 // indirect
-	golang.org/x/net v0.55.0 // indirect
-	golang.org/x/sync v0.20.0 // indirect
-	golang.org/x/sys v0.45.0 // indirect
-	golang.org/x/text v0.37.0 // indirect
+	golang.org/x/net v0.56.0 // indirect
+	golang.org/x/sys v0.46.0 // indirect
+	golang.org/x/text v0.38.0 // indirect
 	golang.org/x/time v0.15.0 // indirect
 	golang.org/x/tools v0.45.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20260406210006-6f92a3bedf2d // indirect
--- image_build.go
+++ image_build.go
@@ -15,21 +15,28 @@
 package morbyd
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
 	"io"
+	"net"
 	"os"
 	"path/filepath"
+	"strings"
+	"sync"
 
 	bkcontrol "github.com/moby/buildkit/api/services/control"
 	bkclient "github.com/moby/buildkit/client"
+	bksession "github.com/moby/buildkit/session"
 	"github.com/moby/buildkit/util/progress/progressui"
 	"github.com/moby/go-archive"
 	"github.com/moby/moby/api/types/jsonstream"
 	"github.com/moby/moby/client"
 	"github.com/moby/moby/client/pkg/jsonmessage"
 	"github.com/moby/patternmatcher/ignorefile"
+	"github.com/thediveo/nonstd/xatomic"
+	"golang.org/x/sync/errgroup"
 	"google.golang.org/protobuf/proto"
 
 	"github.com/thediveo/morbyd/v2/build"
@@ -51,7 +58,11 @@ import (
 // If no build process output writer has been specified using [build.WithOutput]
 // any output (such as build steps, et cetera) will simply be discarded.
 //
-// Use [build.WithBuildKit] to build the image with buildkit.
+// Note: using buildkit ([build.WithBuildKit]) currently is subject to
+// limitations, most notably, registry authentication is not passed on to
+// buildkit; please see also [API /build doesn't pass AuthConfig to BuildKit].
+//
+// [API /build doesn't pass AuthConfig to BuildKit]: https://github.com/moby/moby/issues/48112
 func (s *Session) BuildImage(ctx context.Context, buildctxpath string, opts ...build.Opt) (id string, err error) {
 	bios := build.Options{
 		ImageBuildOptions: client.ImageBuildOptions{
@@ -81,72 +92,159 @@ func (s *Session) BuildImage(ctx context.Context, buildctxpath string, opts ...b
 		}
 		bios.Context = buildCtxTar
 	}
-	// Now initiate the image build, feeding it our tar(r)ed build context
-	// contents.
-	resp, err := s.moby.ImageBuild(ctx, bios.Context, bios.ImageBuildOptions)
-	if err != nil {
-		return "", fmt.Errorf("image build failed, reason: %w", err)
-	}
-	defer func() { _ = resp.Body.Close() }()
 
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-	statech := make(chan *bkclient.SolveStatus, 32)
+	// We use an error wait group as in case of using buildkit we need to juggle
+	// with multiple concurrent tasks that might error out sooner or later and
+	// we need to then abort the other tasks; otherwise we have to wait for them
+	// to all properly wind down before we can return our result.
+	wg, ctx := errgroup.WithContext(ctx)
+	sessionCtx, sessionDone := context.WithCancel(ctx)
+	defer sessionDone()
+
+	var statech chan *bkclient.SolveStatus // only for BuildKit
+	closeStateCh := sync.OnceFunc(func() {
+		if statech == nil {
+			return
+		}
+		close(statech)
+	})
+	defer closeStateCh()
+
 	if bios.Version == "2" {
-		// caller requests BuildKit, so we want to leverage buildkit's client
-		// display and progress UI, instead of being a dilettante. (Or is this
-		// instead spelled "dilloitte" correctly?)
+		// the caller foolishly requests BuildKit and now hell breaks loose. In
+		// order to build non-trivial Dockerfiles using the Docker
+		// daemon-integrated buildkit successfully we need to create a new
+		// buildkit session over a hijacked Docker API connection.
+		buildkitSession, err := bksession.NewSession(sessionCtx, "")
+		if err != nil {
+			return "", fmt.Errorf("buildkit session creation failed, reason: %w", err)
+		}
+		defer func() { _ = buildkitSession.Close() }()
+
+		wg.Go(func() error {
+			// Aaaaarghhhhh!!! This is one of those pitch-dark long-running
+			// function error reporting anti-pattern: in case the dialing fails,
+			// buildkit.Session.Run returns early with an error. Otherwise it
+			// tucks on and later always(!) returns nil. So when is the point of
+			// no return where we won't get any error anymore? No-one knows.
+			//
+			// Where did I saw that anti-pattern before? Riiiight: the podman
+			// native API!
+			//
+			// When the session enters the real "run" phase it won't ever return
+			// anything other than nil, even if the context is cancelled or
+			// times out, of the session gets closed.
+			err := buildkitSession.Run(sessionCtx, func(ctx context.Context, proto string, meta map[string][]string) (net.Conn, error) {
+				return s.Client().(client.APIClient).DialHijack(ctx, "/session", proto, meta)
+			})
+			if sessionCtx.Err() != nil && ctx.Err() == nil {
+				// if only our session was cancelled that is normal behavior, so
+				// we should not report this as an error as we would otherwise
+				// race with any other error return values from our waitgroup's
+				// concurrent go routines.
+				return nil
+			}
+			return err // ...but report "early" errors.
+		})
+		bios.SessionID = buildkitSession.ID()
+		// of course, we want to leverage buildkit's client display and progress
+		// UI, instead of being a dilettante. (Or is this instead spelled
+		// "dilloitte" correctly?)
 		//
 		// Anyway, we create a display and run its processing loop in a
 		// background go routine, feeding it status messages below as we receive
 		// them via auxillary messages through the HTTP response body.
-		var err error
+		//
+		// This processing loop will cleanly terminate when the feeder closes
+		// the status change channel. Cancelling the context will instead result
+		// in an error ... which will be ignored by the waitgroup in case
+		// another of its go routines has failed earlier.
 		bkdisplay, err := progressui.NewDisplay(bios.Out, progressui.AutoMode)
 		if err != nil {
 			return "", fmt.Errorf("buildkit progress UI display creation failed, reason: %w", err)
 		}
-		go func() {
-			_, _ = bkdisplay.UpdateFrom(ctx, statech)
-		}()
+		statech = make(chan *bkclient.SolveStatus, 32)
+
+		wg.Go(func() error {
+			// UpdateFrom returns when either the state change channel has been
+			// closed and drained, or when our context was done/cancelled ...
+			// which happens when either one of the other go routines has failed
+			// returning an error, or the context passed to our BuildImage
+			// method has been done/cancelled.
+			warnings, err := bkdisplay.UpdateFrom(ctx, statech)
+			if err != nil {
+				return err
+			}
+			// If this was a clean return from UpdateFrom, then render the
+			// collected warnings, if any...
+			for _, warning := range warnings {
+				_, _ = bios.Out.Write([]byte(prettyPrintVertexWarning(warning)))
+			}
+			// ...and cancel our error waitgroup-derived sub context; now, as we
+			// can only be done after the state change channel has been closed
+			// there's only the above running buildkit session to terminate.
+			// However, please note that we're still racing with the finishing
+			// BuildImage/DisplayStream go routine returning any potential error
+			// result. However, as we are on the path to success we're returning
+			// a nil error and thus won't ever override the error return value
+			// from Build/ImageDisplayStream. That leaves the buildkit session
+			// go routine, so please see above.
+			sessionDone()
+			return nil
+		})
 	}
 
-	err = jsonmessage.DisplayStream(resp.Body, bios.Out,
-		jsonmessage.WithAuxCallback(func(auxmsg jsonstream.Message) {
-			// buildkit messages are rather complex in that they are
-			// protobuf-encoded and transmitted as aux messages with their
-			// dedicated buildkit aux message ID. See also:
-			// https://github.com/moby/moby/discussions/43788#discussioncomment-13291612
-			// Digging deeper into the buildkit code base brings up the
-			// progressui/client display stuff that feeds on status response
-			// messages.
-			if auxmsg.ID == "moby.buildkit.trace" {
-				if auxmsg.Aux == nil {
+	var idval xatomic.Value[string] // never tickle the race detector
+	wg.Go(func() error {
+		// Now initiate the image build, feeding it our tar(r)ed build context
+		// contents.
+		resp, err := s.moby.ImageBuild(ctx, bios.Context, bios.ImageBuildOptions)
+		if err != nil {
+			return fmt.Errorf("image build failed, reason: %w", err)
+		}
+		defer func() { _ = resp.Body.Close() }()
+		err = jsonmessage.DisplayStream(resp.Body, bios.Out,
+			jsonmessage.WithAuxCallback(func(auxmsg jsonstream.Message) {
+				// buildkit messages are rather complex in that they are
+				// protobuf-encoded and transmitted as aux messages with their
+				// dedicated buildkit aux message ID. See also:
+				// https://github.com/moby/moby/discussions/43788#discussioncomment-13291612
+				// Digging deeper into the buildkit code base brings up the
+				// progressui/client display stuff that feeds on status response
+				// messages.
+				if auxmsg.ID == "moby.buildkit.trace" {
+					if auxmsg.Aux == nil {
+						return
+					}
+					var bkpbmsg []byte
+					if err := json.Unmarshal(*auxmsg.Aux, &bkpbmsg); err != nil {
+						return
+					}
+					var status bkcontrol.StatusResponse
+					if err := proto.Unmarshal(bkpbmsg, &status); err != nil {
+						return
+					}
+					statech <- bkclient.NewSolveStatus(&status)
 					return
 				}
-				var bkpbmsg []byte
-				if err := json.Unmarshal(*auxmsg.Aux, &bkpbmsg); err != nil {
+				// Please note that the image ID is reported using an aux message
+				// with its own embedded JSON message and not directly via an "ID"
+				// JSON message.
+				aux := struct {
+					ID string `json:"ID"`
+				}{}
+				if err := json.Unmarshal(*auxmsg.Aux, &aux); err != nil || aux.ID == "" {
 					return
 				}
-				var status bkcontrol.StatusResponse
-				if err := proto.Unmarshal(bkpbmsg, &status); err != nil {
-					return
-				}
-				statech <- bkclient.NewSolveStatus(&status)
-				return
-			}
-			// Please note that the image ID is reported using an aux message
-			// with its own embedded JSON message and not directly via an "ID"
-			// JSON message.
-			aux := struct {
-				ID string `json:"ID"`
-			}{}
-			if err := json.Unmarshal(*auxmsg.Aux, &aux); err != nil || aux.ID == "" {
-				return
-			}
-			// Pick up the image ID when it floats by ... and is non-zero.
-			id = aux.ID
-		}))
-	return id, err
+				// Pick up the image ID when it floats by ... and is non-zero.
+				idval.Store(aux.ID)
+			}))
+		closeStateCh()
+		return err
+	})
+
+	err = wg.Wait()
+	return idval.Load(), err
 }
 
 // readIgnorePatterns reads the file specified by “name” in .dockerignore
@@ -163,4 +261,40 @@ func readIgnorePatterns(name string) []string {
 		return nil
 	}
 	return patterns
+}
+
+func prettyPrintVertexWarning(warn bkclient.VertexWarning) string {
+	const indentCount = 2
+	var indent = strings.Repeat(" ", indentCount)
+
+	var out bytes.Buffer
+
+	out.WriteString("WARN: ")
+	out.Write(warn.Short)
+	out.WriteRune('\n')
+
+	if warn.SourceInfo != nil && warn.SourceInfo.Filename != "" {
+		out.WriteString(indent)
+		out.WriteString("in: ")
+		out.WriteString(warn.SourceInfo.Filename)
+		out.WriteRune('\n')
+	}
+
+	for _, r := range warn.Range {
+		out.WriteString(indent)
+		out.WriteString("line ")
+		fmt.Fprintf(&out, "%d:%d-%d:%d",
+			r.Start.Line, r.Start.Character,
+			r.End.Line, r.End.Character)
+		out.WriteRune('\n')
+	}
+
+	if warn.URL != "" {
+		out.WriteString(indent)
+		out.WriteString("see also: ")
+		out.WriteString(warn.URL)
+		out.WriteRune('\n')
+	}
+
+	return out.String()
 }
